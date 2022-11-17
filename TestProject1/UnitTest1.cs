085431253cc7a6ec4c25e253e75e2ae77c86f0c4
pareeksachin77 <pareeksachin77@gmail.com>
2022-11-17 16:45:09 +0530

    MoodAnalyser

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject()
+            {
+                string message = null;
+                object expected = new MoodAnalyser(message);
+                object obj = MoodAnalyserFactory.CreateMoodAnalyser("MoodAnalyserAppWithCore.MoodAnalyser", "MoodAnalyser");
+                //expected.Equals(obj);
+                Assert.AreNotEqual(expected, obj);
+
+            }
+            [TestMethod]
+            public void GivenMoodAnalyseClassName_ShouldReturnMoodAnalyseObject_UsingParameterizedConstructor()
+            {
+                object expected = new MoodAnalyser("HAPPY"); //2000 var obj;
+                                                             //MoodAnalyser obj = (MoodAnalyser)expected;
+                object expected1 = expected;
+                object obj = MoodAnalyserFactory.CreateMoodAnalyseUsingParameterizedConstructor("MoodAnalyserAppWithCore.MoodAnalyser", "MoodAnalyser", "HAPPY");
+                // expected.Equals(obj);
+                Assert.AreEqual(expected, expected1);
+            }
+
+            [TestMethod]
+            public void GivenMoodHappy_ShouldReturnHappy()
+            {
+                MoodAnalyser obj = new MoodAnalyser("HAPPY");
+                string result = obj.analyseMood();
+                Assert.AreEqual("HAPPY", result);
+
+            }
+        }
+    }
+}
